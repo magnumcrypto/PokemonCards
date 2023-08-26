c7606332a5b546495853aa01faa67f32ef8bf06c
cryptomagnum <jorge_vankov@hotmail.com>
2023-08-26 20:32:55 +0200

    Primer commit - cryptomagnum

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'PokemonCards';
+  nombre = 'Pokedex';
+  subtitle = 'Hazte con todos!';
+}
